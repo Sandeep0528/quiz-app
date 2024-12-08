0701a0275c28a1a7e766813cb1a055e00107fd1a
unknown <challasandeep05@gmail.com>
2024-12-08 22:19:50 +0530

    file is added

---
+++ src/assets/data.js
@@ -0,0 +1,43 @@
+export const data = [
+  {
+    question: "When setState()  is called inside render() method, which of the following events takes place?",
+
+    option1: "duplicate key error",
+    option2: "repititive output blank on the screen",
+    option3: "Nothing happens",
+    option4: "overstackflow error",
+    ans: 4,
+  },
+  {
+    question: "Which continent has the highest number of countries?",
+    option1: "Asia",
+    option2: "Europe",
+    option3: "North America",
+    option4: "Africa",
+    ans: 4,
+  },
+  {
+    question: "Junk e-mail is also called?",
+    option1: "Spam",
+    option2: "Fake",
+    option3: "Archived",
+    option4: "Bin",
+    ans: 1,
+  },
+  {
+    question: "A computer cannot BOOT if it does not have the?",
+    option1: "Application Software",
+    option2: "Internet",
+    option3: "Operating System",
+    option4: "Mouse",
+    ans: 3,
+  },
+  {
+    question: "First page of Website is termed as?",
+    option1: "Index Page",
+    option2: "Homepage",
+    option3: "Sitemap",
+    option4: "Pen Drive",
+    ans: 2,
+  },
+];
